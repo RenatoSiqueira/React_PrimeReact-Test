83918c2d0c890f13d72a4cca41be21f22a245e36
Renato <renatoelysiqueira@gmail.com>
2020-05-11 10:27:29 -0300

    Some tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
